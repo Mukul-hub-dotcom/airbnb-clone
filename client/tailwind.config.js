ecf741397b00a0f6bb9a7973e527208dd09a48b2
Mukul <ramdevmukul800@gmail.com>
2023-06-23 17:29:52 +0530

    made header and login page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#e41c5a'
+      }
+    },
   },
   plugins: [],
 }
