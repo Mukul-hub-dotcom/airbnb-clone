24d889aec38dac14e98e70c55268ed43b28bdc50
Mukul <ramdevmukul800@gmail.com>
2023-06-24 08:30:35 +0530

    made register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
+import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+axios.defaults.baseURL = "http://localhost:3000";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage/>}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
+        <Route index element={<IndexPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
       </Route>
     </Routes>
   );
