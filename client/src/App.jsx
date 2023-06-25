2c5c8de646241535413921d90bede5a6fbbc6322
Mukul <ramdevmukul800@gmail.com>
2023-06-25 23:36:52 +0530

    added place route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,16 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PlacesPage from "./pages/PlacesPage";
 import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacePage from "./pages/PlacePage";
+import { UserContextProvider } from "./UserContext";
 
-axios.defaults.baseURL = "http://127.0.0.1:3000";
-
+axios.defaults.baseURL = "https://airbnb-clone-gewb.onrender.com";
+axios.defaults.withCredentials=true
 
 function App() {
+  
   return (
+    <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />}></Route>
@@ -21,8 +25,10 @@ function App() {
         <Route path="/account" element={<ProfilePage />}></Route>
         <Route path="/account/places?" element={<PlacesPage />}></Route>
         <Route path="/account/places/new" element={<PlacesFormPage />}></Route>
+        <Route path="/place/:id" element={<PlacePage />}></Route>
       </Route>
     </Routes>
+    </UserContextProvider>
   );
 }
 
